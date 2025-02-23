2ec757b8fac24ba931bf0ea5f921d90f297f3f85
rajgy <rajgy32@gmail.com>
2025-02-23 16:50:17 +0545

    Initial Commit

---
+++ sqlPracticing_localhost.py
@@ -0,0 +1,95 @@
+# Import necessary libraries
+from pyspark import SparkConf, SparkContext  # Spark configuration and context
+from pyspark.sql import SparkSession  # Spark session for DataFrame operations
+import os  # For setting environment variables
+import sys  # For accessing system-specific parameters
+
+# Step 1: Set up environment variables for PySpark
+python_path = sys.executable  # Get the current Python interpreter path
+os.environ['PYSPARK_PYTHON'] = python_path  # Tell PySpark to use this Python interpreter
+os.environ['HADOOP_HOME'] = "hadoop"  # Set Hadoop home directory
+os.environ['JAVA_HOME'] = r'C:\Users\ghimi\.jdks\corretto-1.8.0_442'  # Set Java home directory
+
+# Step 2: Configure Spark
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]")
+sc = SparkContext(conf=conf)
+
+# Initialize SparkSession
+spark = SparkSession.builder \
+    .appName("pyspark") \
+    .getOrCreate()
+
+# Print a message to indicate the program has started
+print("STARTED=============")
+
+#🔴 SQL PRE REQUISITE CODE
+
+
+
+
+
+data = [
+    (0, "06-26-2011", 300.4, "Exercise", "GymnasticsPro", "cash"),
+    (1, "05-26-2011", 200.0, "Exercise Band", "Weightlifting", "credit"),
+    (2, "06-01-2011", 300.4, "Exercise", "Gymnastics Pro", "cash"),
+    (3, "06-05-2011", 100.0, "Gymnastics", "Rings", "credit"),
+    (4, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (5, "02-14-2011", 200.0, "Gymnastics", None, "cash"),
+    (6, "06-05-2011", 100.0, "Exercise", "Rings", "credit"),
+    (7, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (8, "02-14-2011", 200.0, "Gymnastics", None, "cash")
+]
+
+df = spark.createDataFrame(data, ["id", "tdate", "amount", "category", "product", "spendby"])
+df.show()
+
+
+
+
+
+data2 = [
+    (4, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (5, "02-14-2011", 200.0, "Gymnastics", None, "cash"),
+    (6, "02-14-2011", 200.0, "Winter", None, "cash"),
+    (7, "02-14-2011", 200.0, "Winter", None, "cash")
+]
+
+df1 = spark.createDataFrame(data2, ["id", "tdate", "amount", "category", "product", "spendby"])
+df1.show()
+
+
+
+
+data4 = [
+    (1, "raj"),
+    (2, "ravi"),
+    (3, "sai"),
+    (5, "rani")
+]
+
+
+
+cust = spark.createDataFrame(data4, ["id", "name"])
+cust.show()
+
+data3 = [
+    (1, "mouse"),
+    (3, "mobile"),
+    (7, "laptop")
+]
+
+prod = spark.createDataFrame(data3, ["id", "product"])
+prod.show()
+
+
+
+df.createOrReplaceTempView("df")
+df1.createOrReplaceTempView("df1")
+cust.createOrReplaceTempView("cust")
+prod.createOrReplaceTempView("prod")
+
+
+import time
+time.sleep(600)
+
+#localhost:4040
